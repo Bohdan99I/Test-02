594f9f9b40868c1b15b63f5ee83aaff6e45f454e
Bohdan99I <bohdan.voiyov@gmail.com>
2023-11-17 21:33:24 +0200

    0.3

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import { NavigationContainer } from '../Navigation/Navigation.styled';
+import { NavLink } from 'react-router-dom'; 
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <NavLink to="news">News</NavLink>
+      <NavLink to="/">Find Pet</NavLink>
+      <NavLink to="friends">Our Friends</NavLink>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
---
+++ src/shared/styles/fonts.js
@@ -0,0 +1,57 @@
+import ManropeRegular from "../fonts/Manrope-Regular.ttf";
+import ManropeSemiBold from "../fonts/Manrope-SemiBold.ttf";
+import ManropeBold from "../fonts/Manrope-Bold.ttf";
+import ManropeExtraBold from "../fonts/Manrope-ExtraBold.ttf";
+import ManropeMedium from "../fonts/Manrope-Medium.ttf";
+import InterRegular from "../fonts/Inter-Regular.ttf";
+import InterMedium from "../fonts/Inter-Medium.ttf";
+import PoppinsRegular from "../fonts/Poppins-Regular.ttf";
+
+export const fonts = [
+  {
+    fontFamily: "Manrope-SemiBold",
+    fontWeight: 600,
+    fontUrl: `${ManropeSemiBold}`,
+  },
+
+  {
+    fontFamily: "Manrope-Regular",
+    fontWeight: 400,
+    fontUrl: `${ManropeRegular}`,
+  },
+  {
+    fontFamily: "Manrope-Bold",
+    fontWeight: 700,
+    fontUrl: `${ManropeBold}`,
+  },
+
+  {
+    fontFamily: "Manrope-ExtraBold",
+    fontWeight: 800,
+    fontUrl: `${ManropeExtraBold}`,
+  },
+
+  {
+    fontFamily: "Manrope-Medium",
+    fontWeight: 500,
+    fontUrl: `${ManropeMedium}`,
+  },
+
+  {
+    fontFamily: "Inter-Regular",
+    fontWeight: 400,
+    fontUrl: `${InterRegular}`,
+  },
+
+  {
+    fontFamily: "Inter-Medium",
+    fontWeight: 500,
+    fontUrl: `${InterMedium}`,
+  },
+
+  {
+    fontFamily: "Poppins-Regular",
+    fontWeight: 400,
+    fontUrl: `${PoppinsRegular}`,
+  },
+];
